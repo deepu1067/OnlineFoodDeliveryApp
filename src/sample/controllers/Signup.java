cb28c95d4959b1d5e79c393b951ce4151866aa6b
deepu1067 <dshahadat10@gmail.com>
2021-09-10 21:41:36 +0600

    back feature

--- src/sample/controllers/Signup.java
+++ src/sample/controllers/Signup.java
@@ -1,5 +1,31 @@
 package sample.controllers;
 
-public class Signup {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class Signup {
+    private Parent parent;
+    private Stage stage;
+    private Scene scene;
+
+
+    public void backButton(MouseEvent mouseEvent) {
+        try {
+            parent = FXMLLoader.load(getClass().getResource("../ui/main.fxml"));
+            scene = new Scene(parent);
+            stage = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
