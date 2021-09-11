b81483578c9d0e78764c7e78e2c4e8e1dccde72b
deepu1067 <dshahadat10@gmail.com>
2021-09-11 21:18:44 +0600

    signup info saving

---
+++ src/sample/extraClasses/User.java
@@ -0,0 +1,11 @@
+package sample.extraClasses;
+
+public class User{
+    public String fName;
+    public String lName;
+    public String email;
+    public String username;
+    public String pass;
+    public CreditCard cc;
+    public String address;
+}
