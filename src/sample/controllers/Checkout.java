434f854acc0575071159b649273b13c4dc01e24a
SudiptoMondal25 <smondal201184@bscse.uiu.ac.bd>
2021-10-08 11:46:50 +0600

    Checkout fxml page complete UI

--- src/sample/controllers/Checkout.java
+++ src/sample/controllers/Checkout.java
@@ -1,4 +1,25 @@
 package sample.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class Checkout {
+    private BufferedWriter writer;
+    private BufferedReader reader;
+    @FXML
+    Button orderPlaceBtn;
+
+    public void orderPlaceBtn(javafx.event.ActionEvent actionEvent) {
+        try {
+            writer.write("Your order has been placed. Thank you!\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
