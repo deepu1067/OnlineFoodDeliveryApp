815c6cd5b2f3a2645512bacdb0c7f9389ef07485
SudiptoMondal25 <smondal201184@bscse.uiu.ac.bd>
2021-10-10 01:19:00 +0600

    Checkout fxml page complete UI & Class 2

--- src/sample/controllers/Checkout.java
+++ src/sample/controllers/Checkout.java
@@ -2,17 +2,24 @@ package sample.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
 import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.security.PrivateKey;
 
 public class Checkout {
     private BufferedWriter writer;
     private BufferedReader reader;
     @FXML
     Button orderPlaceBtn;
+
+    @FXML
+    public ChoiceBox<String> paymentMethod;
+    public String[] methods = {"Cash", "Visa", "Mastercard"};
+
 
     public void orderPlaceBtn(javafx.event.ActionEvent actionEvent) {
         try {
