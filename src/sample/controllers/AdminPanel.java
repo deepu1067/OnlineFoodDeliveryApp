830a0d726e515d1bd2c2e15361d680abdf16df72
SudiptoMondal25 <smondal201184@bscse.uiu.ac.bd>
2021-10-10 19:55:40 +0600

    Admin Panel UI

---
+++ src/sample/controllers/AdminPanel.java
@@ -0,0 +1,20 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class AdminPanel {
+    @FXML
+    private Button deleteItem;
+    @FXML
+    private Button home;
+
+    void deleteItem(ActionEvent event){
+
+    }
+    void home(ActionEvent event){
+
+    }
+}
