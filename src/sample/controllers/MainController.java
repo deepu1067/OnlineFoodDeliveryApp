cb0a083f1b83440a52bc39617bdca9a10cad31c5
deepu1067 <dshahadat10@gmail.com>
2021-10-11 15:48:54 +0600

    Minor fix-2

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -28,7 +29,10 @@ public class MainController {
 
 
     @FXML
-    TextField username, password;
+    TextField username;
+
+    @FXML
+    PasswordField password;
 
     @FXML
     Label errorUsername, errorPass;
